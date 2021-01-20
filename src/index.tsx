32fae89feefb19a992f83edf8d5b2da5377f4999
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-20 18:27:35 +0200

    Wrap app in Browser router

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 
@@ -10,9 +11,11 @@ import store, { rrfProps } from 'store/store';
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
-      </ReactReduxFirebaseProvider>
+      <BrowserRouter>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <App />
+        </ReactReduxFirebaseProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
