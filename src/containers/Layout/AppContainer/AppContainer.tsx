723fb6f8617c9e81216eef4f2958d473a3a51699
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-23 17:48:10 +0200

    Adjust application Container settings

--- src/containers/Layout/AppContainer/AppContainer.tsx
+++ src/containers/Layout/AppContainer/AppContainer.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
 
 import { HeaderContainer } from 'containers/Layout/HeaderContainer/HeaderContainer';
+import { Container } from 'components/utility';
+
+import './AppContainer.css';
 
 export const AppContainer: FC = ({ children }) => {
   return (
     <>
       <HeaderContainer />
-      <main>{children}</main>
+      <main className="main">
+        <Container>{children}</Container>
+      </main>
     </>
   );
 };
