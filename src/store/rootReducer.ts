98a096dbb5ecd4cd4d8e8d7bac27eb318652f4fd
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-24 10:44:48 +0200

    Add auth slice

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -3,9 +3,11 @@ import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
 import testReducer from 'store/slices/testSlice';
+import authReducer from 'store/slices/authSlice';
 
 const rootReducer = combineReducers({
   test: testReducer,
+  auth: authReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
 });
