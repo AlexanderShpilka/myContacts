32966013899e152295869865f08ec004bd29ad1c
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-24 08:02:11 +0200

    Add FormContainer utility component

--- src/components/utility/index.tsx
+++ src/components/utility/index.tsx
@@ -5,3 +5,15 @@ import './styles.css';
 export const Container: FC = ({ children }) => {
   return <div className="container">{children}</div>;
 };
+
+interface FormContainerProps {
+  title: string;
+}
+export const FormContainer: FC<FormContainerProps> = ({ title, children }) => {
+  return (
+    <div className="form-container">
+      <h1 className="form-title">{title}</h1>
+      {children}
+    </div>
+  );
+};
