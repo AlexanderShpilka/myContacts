19c2f480510403be2aacfefb4ea453be1f4fa6c8
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-25 08:08:26 +0200

    Remove test store slice

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -2,11 +2,9 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { firebaseReducer } from 'react-redux-firebase';
 import { firestoreReducer } from 'redux-firestore';
 
-import testReducer from 'store/slices/testSlice';
 import authReducer from 'store/slices/authSlice';
 
 const rootReducer = combineReducers({
-  test: testReducer,
   auth: authReducer,
   firebase: firebaseReducer,
   firestore: firestoreReducer,
