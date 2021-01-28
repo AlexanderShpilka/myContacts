aeb55887b282bcdd3f7a8080b6233fca14fda021
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-28 22:45:41 +0200

    Add feedback block for Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,13 +6,19 @@ import './Modal.css';
 interface ModalProps {
   open: boolean;
   title: string;
+  feedback?: string;
 }
 
-export const Modal: FC<ModalProps> = ({ open, title, children }) => {
+export const Modal: FC<ModalProps> = ({ open, title, feedback, children }) => {
+  const feedbackClassName = feedback ? 'modal-feedback modal-feedback--show' : 'modal-feedback';
+
   return createPortal(
     <div className={`${open ? 'modal modal--open' : 'modal'}`}>
       <h1 className="modal-title">{title}</h1>
       {children}
+      <div className="modal-feedback-wrapper">
+        <p className={feedbackClassName}>{feedback}</p>
+      </div>
     </div>,
     document.getElementById('root-modal') as HTMLElement,
   );
