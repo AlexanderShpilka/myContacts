44ead5044979670863c75473fab6dda6c18a0e2a
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-30 08:39:10 +0200

    Modify Contacts page to include controls and list of contacts

--- src/pages/contacts/Contacts/Contacts.tsx
+++ src/pages/contacts/Contacts/Contacts.tsx
@@ -1,3 +1,15 @@
+import { ContactsControls } from 'containers/contacts/ContactsControls/ContactsControls';
+import { ContactList } from 'containers/contacts/ContactList/ContactList';
+
+import './Contacts.css';
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <div className="contacts">
+      <div className="contacts-wrapper">
+        <ContactsControls />
+        <ContactList />
+      </div>
+    </div>
+  );
 };
