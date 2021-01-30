e779c81bd9efda5c5cb93c5b184e431f622318ea
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-30 09:22:45 +0200

    Store name filter in Contacts container

--- src/pages/contacts/Contacts/Contacts.tsx
+++ src/pages/contacts/Contacts/Contacts.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
 import { ContactsControls } from 'containers/contacts/ContactsControls/ContactsControls';
 import { ContactList } from 'containers/contacts/ContactList/ContactList';
 
 import './Contacts.css';
 
 export const Contacts = () => {
+  const [nameFilter, setNameFilter] = useState('');
+
   return (
     <div className="contacts">
       <div className="contacts-wrapper">
-        <ContactsControls />
+        <ContactsControls
+          value={nameFilter}
+          onNameFilterChangeHandler={({ target }) => setNameFilter((target as HTMLInputElement).value)}
+        />
         <ContactList />
       </div>
     </div>
