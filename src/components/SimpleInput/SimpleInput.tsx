add1284871e9d02fd5d8c9204204a034972aceba
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-30 09:24:07 +0200

    Make Input as controlled component

--- src/components/SimpleInput/SimpleInput.tsx
+++ src/components/SimpleInput/SimpleInput.tsx
@@ -1,11 +1,24 @@
+import { FormEvent } from 'react';
+
 import './SimpleInput.css';
 
 interface SimpleInputProps {
   type: 'text' | 'email';
   name: string;
   placeholder?: string;
+  value: string;
+  onChangeHandler: (evt: FormEvent<HTMLInputElement>) => void;
 }
 
-export const SimpleInput = (props: SimpleInputProps) => {
-  return <input className="simple-input" {...props} />;
+export const SimpleInput = ({ type, name, placeholder, value, onChangeHandler }: SimpleInputProps) => {
+  return (
+    <input
+      className="simple-input"
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChangeHandler}
+    />
+  );
 };
