e6ec3536a5a8b1b8db2745e761925c08c49703aa
AlexanderShpilka <alexander.shpilka@gmail.com>
2021-01-31 19:04:46 +0200

    Add search by full name functionality

--- src/containers/contacts/ContactList/ContactList.tsx
+++ src/containers/contacts/ContactList/ContactList.tsx
@@ -11,7 +11,11 @@ import { RootState } from 'store/rootReducer';
 import { ContactWithId, contactsCleanUp, deleteContact } from 'store/slices/contactsSlice';
 import './ContactList.css';
 
-export const ContactList = () => {
+interface ContactListProps {
+  searchQuery: string;
+}
+
+export const ContactList = ({ searchQuery }: ContactListProps) => {
   const [contactToEdit, setContactToEdit] = useState<ContactWithId | null>(null);
   const [contactToDelete, setContactToDelete] = useState<ContactWithId | null>(null);
 
@@ -33,17 +37,22 @@ export const ContactList = () => {
   }
 
   const visibleContacts = useMemo(() => {
-    return [...contacts].sort((contA, contB) => {
-      const contAFullName = `${contA.firstName}${contA.lastName ? contA.lastName : ''}`;
-      const contBFullName = `${contB.firstName}${contB.lastName ? contB.lastName : ''}`;
-      return contAFullName.localeCompare(contBFullName);
-    });
-  }, [contacts]);
+    return contacts
+      ?.filter((contact) => {
+        const contactFullName = `${contact.firstName}${contact.lastName ? contact.lastName : ''}`;
+        return contactFullName.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase());
+      })
+      .sort((contA, contB) => {
+        const contAFullName = `${contA.firstName}${contA.lastName ? contA.lastName : ''}`;
+        const contBFullName = `${contB.firstName}${contB.lastName ? contB.lastName : ''}`;
+        return contAFullName.localeCompare(contBFullName);
+      });
+  }, [contacts, searchQuery]);
 
   return (
     <>
       {content}
-      {visibleContacts.map((contact) => (
+      {visibleContacts?.map((contact) => (
         <div className="contact-list-card-wrapper" key={contact.id}>
           <ContactCard
             {...contact}
--- src/pages/contacts/Contacts/Contacts.tsx
+++ src/pages/contacts/Contacts/Contacts.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import debounce from 'lodash/debounce';
 
 import { ContactsControls } from 'containers/contacts/ContactsControls/ContactsControls';
 import { ContactList } from 'containers/contacts/ContactList/ContactList';
@@ -7,15 +8,24 @@ import './Contacts.css';
 
 export const Contacts = () => {
   const [nameFilter, setNameFilter] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const setSearchQueryWithDebounce = useCallback(
+    debounce((value: string): void => setSearchQuery(value), 1000),
+    [],
+  );
 
   return (
     <div className="contacts">
       <div className="contacts-wrapper">
         <ContactsControls
           value={nameFilter}
-          onNameFilterChangeHandler={({ target }) => setNameFilter((target as HTMLInputElement).value)}
+          onNameFilterChangeHandler={({ target }) => {
+            setNameFilter((target as HTMLInputElement).value);
+            setSearchQueryWithDebounce((target as HTMLInputElement).value);
+          }}
         />
-        <ContactList />
+        <ContactList searchQuery={searchQuery} />
       </div>
     </div>
   );
